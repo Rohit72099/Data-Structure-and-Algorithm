608e96389f63b588ef5db7b8fcf225b8ad055bfa
Rohit72099 <rk464152@gmail.com>
2024-06-18 20:10:54 +0530

    1.cpp added successfully

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int n;
+    // cin>>n;
+    for(int i=0;i<5;i++){
+        cout<<"*";
+        for(int i=0;i<4;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+return 0;
+}
