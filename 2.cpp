9a2a719d6896576cc4cdcd77c71def6d9fd69840
Rohit72099 <rk464152@gmail.com>
2024-06-19 21:53:41 +0530

    2.cpp added successfully

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        // cout<<"*";
+        // cout<<endl;
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+return 0;
+}
